acae90546169cf99b296e3458a5299da4623ec97
govadaishAsnn <b123ishan@gmail.com>
2020-11-22 11:43:26 +0530

    final

--- sketch.js
+++ sketch.js
@@ -35,6 +35,9 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    log6= new Log(230,180,80,PI/2);
+    slingshot= new Slingshot(bird.body,log6.body);
+
 
 }
 
@@ -61,4 +64,8 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    log6.display();
+    slingshot.display();
+    
 }
